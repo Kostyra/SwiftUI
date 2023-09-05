b300644468ba6dbae5d58b72b14c09e65b47864f
KAFilippov <kostya.filippov.88@mail.ru>
2023-09-05 15:28:58 +0300

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Юлия Филиппова on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
